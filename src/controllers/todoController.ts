4239766afe0c551c9841ce9bc84f5ebfa59dab7b
Benjamin Pearse <ben.pearse@ymail.com>
2025-04-13 17:59:20 +0100

    refactor: move route logic to controller file

---
+++ src/controllers/todoController.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import { pool } from "../db/index";
+
+export const getAllTodos = async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query("SELECT * FROM todos;");
+    res.json(result.rows);
+  } catch (err) {
+    console.error("Database query error", err);
+    res.status(500).send("Internal server error");
+  }
+};
+
+export const createTodo = async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(
+      `INSERT INTO todos (id, name, complete) VALUES (uuid_generate_v4(), '${req.body.name}', false);`
+    );
+    res.status(201).json(result.rows);
+  } catch (err) {
+    console.error("Database query error", err);
+    res.status(500).send("Internal server error");
+  }
+};
+
+export const updateTodo = async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(
+      `UPDATE todos SET name = '${req.body.name}', complete = ${req.body.complete} WHERE id = '${req.params.id}';`
+    );
+    res.status(201).json(result.rows);
+  } catch (err) {
+    console.error("Database query error", err);
+    res.status(500).send("Internal server error");
+  }
+};
+
+export const deleteTodo = async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(
+      `DELETE FROM todos WHERE id = '${req.params.id}';`
+    );
+    res.json({ message: "Todo deleted" });
+  } catch (err) {
+    console.error("Database query error", err);
+    res.status(500).send("Internal server error");
+  }
+};
--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,53 +1,20 @@
+import {
+  createTodo,
+  deleteTodo,
+  getAllTodos,
+  updateTodo,
+} from "../controllers/todoController";
+
 const express = require("express");
-import { Request, Response } from "express";
 const router = express.Router();
-import { pool } from "../db/index";
 
 // Routes
-router.get("/", async (req: Request, res: Response) => {
-  try {
-    const result = await pool.query("SELECT * FROM todos;");
-    res.json(result.rows);
-  } catch (err) {
-    console.error("Database query error", err);
-    res.status(500).send("Internal server error");
-  }
-});
+router.get("/", getAllTodos);
 
-router.post("/", async (req: Request, res: Response) => {
-  try {
-    const result = await pool.query(
-      `INSERT INTO todos (id, name, complete) VALUES (uuid_generate_v4(), '${req.body.name}', false);`
-    );
-    res.status(201).json(result.rows);
-  } catch (err) {
-    console.error("Database query error", err);
-    res.status(500).send("Internal server error");
-  }
-});
+router.post("/", createTodo);
 
-router.put("/:id", async (req: Request, res: Response) => {
-  try {
-    const result = await pool.query(
-      `UPDATE todos SET name = '${req.body.name}', complete = ${req.body.complete} WHERE id = '${req.params.id}';`
-    );
-    res.status(201).json(result.rows);
-  } catch (err) {
-    console.error("Database query error", err);
-    res.status(500).send("Internal server error");
-  }
-});
+router.put("/:id", updateTodo);
 
-router.delete("/:id", async (req: Request, res: Response) => {
-  try {
-    const result = await pool.query(
-      `DELETE FROM todos WHERE id = '${req.params.id}';`
-    );
-    res.json({ message: "Todo deleted" });
-  } catch (err) {
-    console.error("Database query error", err);
-    res.status(500).send("Internal server error");
-  }
-});
+router.delete("/:id", deleteTodo);
 
 export default router;
